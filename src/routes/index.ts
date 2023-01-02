b3235911b50701d40caac5cc20968aa09a9d5c9d
Nour-Eldeen <noureldeen.nasser@tmentors.com>
2023-01-02 23:10:21 +0200

    the orders

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import usersRouter from "./api/users.routes";
+import theOrdersRoutes from "./api/orderRoutes"
 
 const themainRoutes = Router();
 
 themainRoutes.use("/users", usersRouter);
+themainRoutes.use("/orders",theOrdersRoutes);
+
 
 export default themainRoutes;
